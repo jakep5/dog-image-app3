09e63cc62c7832e77e5d976fedad9a388d727c1a
jakep5 <jakepagel1@yahoo.com>
2019-10-01 17:52:20 -0500

    finalized application

---
+++ index.js
@@ -0,0 +1,33 @@
+function watchForm() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        var breed = document.getElementById("breed").value;
+        getBreedImage(breed);
+    })
+}
+
+function getBreedImage(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then (response => response.json())
+        .then (responseJson =>
+            displayResults(responseJson))
+        .catch(error => alert ('The input breed could not be found.'));
+}
+
+function displayResults(responseJson) {
+        console.log(responseJson.message);
+        if ((responseJson.message).includes('Breed not found')){
+            alert ('The input breed could not be found')
+        }
+        else {
+        $("div.images").append (
+            `<img src="${responseJson.message}" class="dogImages">`
+
+        )
+        };
+}
+
+$(function() {
+    console.log('Form is ready! Waiting for submit');
+    watchForm();
+});
